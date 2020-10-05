b3d5db94dbb1807c397a075f8bcb406b8b8830f4
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-10-05 23:30:52 +0530

    Add files via upload

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,135 +1,147 @@
-// acquiring [express]
-const express=require('express');
-// creating [router]
-const router=express.Router();
-const mongoose=require('mongoose');
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 
+const Product = require("../models/product");
 
-const Product=require('../models/product');
-
-// registering [routes]
-
-//[GET] REQUEST
-router.get('/', (req,res,next) => {
-   Product
-   .find()
-   .exec()
-   .then(docs =>{
-       console.log(docs);
-       res.status(200).json(docs);
+router.get("/", (req, res, next) => {
+  Product.find()
+    .select("name price _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          };
+        })
+      };
+      //   if (docs.length >= 0) {
+      res.status(200).json(response);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
     })
-   .catch(err =>{
-       console.log(err);
-       res.status(500).json({
-           error:err
-       });
-   });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
-
-//[POST] REQUEST
-router.post('/', (req,res,next) => {
-   const product= new Product({
-        _id:new mongoose.Types.ObjectId(),
-        name:req.body.name,
-        price:req.body.price
-    });
-    //save will store this product in the database
-    
-    product
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
     .save()
-    .then(result =>{
-        console.log(result);
-        res.status(201).json({
-            message:"handle post request to /products",
-            createdProduct: product
-        }); 
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:3000/products/" + result._id
+            }
+        }
+      });
     })
-    .catch(err =>{
-        console.log(err)
-        res.status(500).json({
-            error:err
-        })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
-     
-   
- });
+});
 
-
- // [ELEMENT GET] REQUEST
-
- router.get('/:productId',(req,res,next) => { 
-     const id=req.params.productId;
-     Product.findById(id)
-     .exec()
-     .then(doc =>{
-         console.log("from datatbase",doc);
-         res.status(200).json(doc);
-        })
-     .catch(err =>{
-         console.log(err);
-         res.status(500).json({
-             error: err
-         });
-
-     });
-  });
-
-
-  // [patch] route 
-   
-  router.patch('/:productId',(req,res,next) =>{
-     
-    const id=req.params.productId;
-
-    const updateOps = {};
-    // this only changes what is required no everything
-
-    for( const ops of req.body)
-    {
-        updateOps[ops.propName] = ops.value;
-
-    }
-
-    Product
-    .update({id: id},{ $set:updateOps})
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .select('name price _id')
     .exec()
-    .then(result =>{
-        console.log(result);
-        res.status(200).json(result);
-    })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json({
-            error: err
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json({
+            product: doc,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/products'
+            }
         });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
+});
 
-  });
-
-
-  //[delete ] route
-   
-  router.delete('/:productId',(req,res,next) =>{
-    const id=req.params.productId;
-
-    Product.remove({_id : id})
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
     .exec()
-    .then(res =>{
-        res.status(200).json(result);
+    .then(result => {
+      res.status(200).json({
+          message: 'Product updated',
+          request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + id
+          }
+      });
     })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-
-    })
-  
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
-  
+});
 
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+          message: 'Product deleted',
+          request: {
+              type: 'POST',
+              url: 'http://localhost:3000/products',
+              body: { name: 'String', price: 'Number' }
+          }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
-
-
- module.exports=router;
+module.exports = router;
